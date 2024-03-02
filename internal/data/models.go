bbe395a726983404cc3d8745b0e1e00e117d281f
= <=>
2024-03-02 23:31:56 +0200

    hooked up users model

--- internal/data/models.go
+++ internal/data/models.go
@@ -47,11 +47,13 @@ type Models struct {
 		Delete(id int64) error
 		GetAll(title string, genres []string, filter Filter) ([]*Movie, Metadata, error)
 	}
+	Users UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }
 
